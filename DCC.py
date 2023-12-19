edbdc37a3bf8006dc1e62c3c96812e630d69b7bd
debit <151398786+deb-its@users.noreply.github.com>
2023-12-19 20:34:12 +0800

    Code Cleanup
    Removed unnecessary comments generated by CHATGPT & fixed code structure

--- DCC.py
+++ DCC.py
@@ -11,13 +11,12 @@ def is_discord_running():
             return True
     return False
 
-def delete_discord_cache(logs_directory, progress_var, status_label):
+def cache_clean(progress_var, status_label):
     try:
         if is_discord_running():
             status_label.config(text="Please close discord before clearing cache.")
             return
 
-        # Specify the paths to Discord cache directories
         cache_paths = [
             os.path.join(os.getenv("APPDATA"), "discord", "Cache"),
             os.path.join(os.getenv("APPDATA"), "discord", "Code Cache"),
@@ -29,9 +28,7 @@ def delete_discord_cache(logs_directory, progress_var, status_label):
 
         for index, cache_path in enumerate(cache_paths):
             try:
-                # Check if the directory exists before attempting to delete
                 if os.path.exists(cache_path):
-                    # Extract the last part of the path (e.g., "Cache", "Code Cache", "GPUCache")
                     cache_name = os.path.basename(cache_path)
                     shutil.rmtree(cache_path)
                     progress_value = (index + 1) / total_directories * 100
@@ -43,7 +40,6 @@ def delete_discord_cache(logs_directory, progress_var, status_label):
             except Exception as e:
                 status_label.config(text=f"Error deleting {cache_path}: {e}")
 
-        # Display the success message only if at least one directory was deleted
         if deleted_at_least_one_directory:
             status_label.config(text="Discord Cache Cleaned!")
 
@@ -53,7 +49,7 @@ def delete_discord_cache(logs_directory, progress_var, status_label):
 root = tk.Tk()
 root.title("Discord Cache Cleaner")
 
-delete_button = tk.Button(root, text="Clear Cache", command=lambda: Thread(target=delete_discord_cache, args=(os.path.expandvars(r"%appdata%\discord\logs"), progress_var, status_label)).start())
+delete_button = tk.Button(root, text="Clear Cache", command=lambda: Thread(target=cache_clean, args=(progress_var, status_label)).start())
 progress_var = tk.DoubleVar()
 progress_bar = ttk.Progressbar(root, variable=progress_var, length=290, mode="determinate")
 status_label = tk.Label(root, text="")
